6269b157183c5be587a01aa45add2dff02e0775f
adrian-jaimes2430 <75454823+adrian-jaimes2430@users.noreply.github.com>
2026-02-08 17:39:50 -0500

    Update auth.service.ts

--- frontend/src/auth/auth.service.ts
+++ frontend/src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import api from "@/services/api";
+import api from "./services/api";
 
 export async function login(email: string, password: string) {
   const res = await api.post("/auth/login-jwt", { email, password });
