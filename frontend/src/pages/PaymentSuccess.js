d522b85569c32d0bd83cf5ae6e1e93bad1687e90
adrian-jaimes2430 <75454823+adrian-jaimes2430@users.noreply.github.com>
2026-02-08 17:47:37 -0500

    Update PaymentSuccess.js

--- frontend/src/pages/PaymentSuccess.js
+++ frontend/src/pages/PaymentSuccess.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import axios from 'axios';
-import { Button } from '@/components/ui/button';
+import { Button } from './components/ui/button';
 import { CheckCircle, Loader } from 'lucide-react';
 import { toast } from 'sonner';
 
